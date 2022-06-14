2a00b4b4d166da3f02c3a9cf46a39665a70dc67c
gavvahar <nihargavva@gmail.com>
2022-06-14 15:32:38 -0400

    logic for computers selection

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ let computerSelection = Math.random(); //Randomly generate a number
 let rps = ["rock", "paper", "scissors"];
 
 //Conditional logic goes here to determine the computers selection
-
+switch(true)
+{
+    case (computerSelection <= 0.34):
+        computerSelection = rps[0];
+        break;
+    case (computerSelection <= 0.67):
+        computerSelection = rps[1];
+        break;
+    default:
+        computerSelection = rps[2];
+}
 //<--Code goes here-->
 
 //Function to compare the userSelection and the computerSelection to determine the winner
